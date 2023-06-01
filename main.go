f60e1773949c4e13501d375e445f7bea283672f1
Christos Ploutarcou <cploutarchou@gmail.com>
2023-06-01 21:41:53 +0300

    refactor

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"strings"
-	"sync"
-	"time"
-	
-	"github.com/Shopify/sarama"
-	"github.com/cploutarchou/go-kafka-rest/kafka"
-	
 	"github.com/cploutarchou/go-kafka-rest/controllers"
+	"log"
+	"strings"
+	"time"
+
 	"github.com/cploutarchou/go-kafka-rest/initializers"
 	"github.com/cploutarchou/go-kafka-rest/middleware"
 	"github.com/gofiber/fiber/v2"
@@ -19,22 +14,11 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-type MessagePayload struct {
-	Topic string `json:"topic"`
-	Data  string `json:"data"`
-	Key   string `json:"key"`
-}
-
-var (
-	workerPoolSize = 100 // Number of workers in the pool
-	workerPool     = make(chan struct{}, workerPoolSize)
-	wg             sync.WaitGroup   // WaitGroup to wait for workers to finish
-	mutex          sync.Mutex       // Mutex to protect shared resources
-	messageQueue   []MessagePayload // Shared message queue
-	producer       *kafka.Producer  // Kafka producer 
-)
+var controller *controllers.Controller
 
 func init() {
+
+	controller = controllers.NewController()
 	config, err := initializers.LoadConfig(".")
 	if err != nil {
 		log.Fatalf("Failed to load environment variables! \n%s", err.Error())
@@ -45,8 +29,7 @@ func init() {
 		log.Fatalf("Failed to load environment variables! \n%s", "KAFKA_BROKER is not set")
 	}
 	brokers := strings.Split(config.KafkaBrokers, ",")
-	producer, err = kafka.NewProducer(brokers, nil, myProducerFactory)
-	
+	controller.SetBrokers(brokers)
 	if err != nil {
 		log.Fatalf("Failed to connect to Kafka! \n%s", err.Error())
 	}
@@ -54,7 +37,7 @@ func init() {
 
 func setupApp() *fiber.App {
 	app := fiber.New()
-	
+
 	app.Use(logger.New())
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     "http://localhost:3000",
@@ -62,7 +45,7 @@ func setupApp() *fiber.App {
 		AllowMethods:     "GET, POST",
 		AllowCredentials: true,
 	}))
-	
+
 	app.Use(func(c *fiber.Ctx) error {
 		start := time.Now()
 		if err := c.Next(); err != nil {
@@ -79,28 +62,29 @@ func setupApp() *fiber.App {
 		log.Printf("Execution time: %v", time.Since(start))
 		return nil
 	})
-	
+	// set brokers as fiber app variable
+
 	return app
 }
 
 func setupMicro() *fiber.App {
 	micro := fiber.New()
-	
+
 	micro.Get("/healthchecker", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
 			"status":  "success",
 			"message": "JWT Authentication with Golang, Fiber, and GORM",
 		})
 	})
-	
+
 	micro.Route("/auth", func(router fiber.Router) {
-		router.Post("/register", controllers.SignUpUser)
-		router.Post("/login", controllers.SignInUser)
-		router.Get("/logout", middleware.DeserializeUser, controllers.LogoutUser)
-		router.Post("/receive-message", receiveMessage)
+		router.Post("/register", controller.User.SignUpUser)
+		router.Post("/login", controller.User.SignInUser)
+		router.Get("/logout", middleware.DeserializeUser, controller.User.LogoutUser)
+		router.Post("/receive-message", controller.User.ReceiveMessage)
 	})
-	
-	micro.Get("/users/me", middleware.DeserializeUser, controllers.GetMe)
+
+	micro.Get("/users/me", middleware.DeserializeUser, controller.User.GetMe)
 	micro.All("*", func(c *fiber.Ctx) error {
 		path := c.Path()
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
@@ -108,99 +92,15 @@ func setupMicro() *fiber.App {
 			"message": fmt.Sprintf("Path: %v does not exist on this server", path),
 		})
 	})
-	
+
 	return micro
 }
 
 func main() {
 	app := setupApp()
 	micro := setupMicro()
-	
+
 	app.Mount("/api", micro)
-	
+
 	log.Fatal(app.Listen(":8045"))
-}
-
-func receiveMessage(c *fiber.Ctx) error {
-	// Parse JSON payload
-	var messagePayload MessagePayload
-	err := c.BodyParser(&messagePayload)
-	if err != nil {
-		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
-			"status":  "fail",
-			"message": "Invalid request payload",
-		})
-	}
-	
-	// Validate the message payload
-	if messagePayload.Topic == "" {
-		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
-			"status":  "fail",
-			"message": "Topic is missing",
-		})
-	}
-	
-	if messagePayload.Data == "" {
-		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
-			"status":  "fail",
-			"message": "Data is missing",
-		})
-	}
-	
-	// Add the message to the queue
-	mutex.Lock()
-	messageQueue = append(messageQueue, messagePayload)
-	mutex.Unlock()
-	
-	// Notify a worker to process the message
-	workerPool <- struct{}{}
-	wg.Add(1)
-	
-	go func() {
-		defer func() {
-			// Release the worker and mark the task as done
-			<-workerPool
-			wg.Done()
-		}()
-		
-		// Process messages from the queue
-		for {
-			// Acquire a message from the queue
-			mutex.Lock()
-			if len(messageQueue) == 0 {
-				mutex.Unlock()
-				break
-			}
-			message := messageQueue[0]
-			messageQueue = messageQueue[1:]
-			mutex.Unlock()
-			
-			// Process the message
-			producer.SendMessageAsync(message.Topic, message.Data, message.Key)
-			log.Printf("Kafka message produced! Topic: %s\n", message.Topic)
-		}
-	}()
-	
-	return c.Status(http.StatusOK).JSON(fiber.Map{
-		"status":  "success",
-		"message": "Message received and added to the processing queue",
-	})
-}
-
-func myProducerFactory(brokers []string, config *sarama.Config) (sarama.SyncProducer, sarama.AsyncProducer, error) {
-	syncProducer, err := sarama.NewSyncProducer(brokers, config)
-	if err != nil {
-		return nil, nil, err
-	}
-	
-	asyncProducer, err := sarama.NewAsyncProducer(brokers, config)
-	if err != nil {
-		err := syncProducer.Close()
-		if err != nil {
-			return nil, nil, err
-		}
-		return nil, nil, err
-	}
-	
-	return syncProducer, asyncProducer, nil
 }
